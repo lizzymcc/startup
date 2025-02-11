6f2d7f40f24b964889505773fedd1317f22774e0
Lizzy McClellan <lizzymcc@byu.edu>
2025-02-11 15:34:21 -0700

    i think most of this is working navigation-wise EXCEPT for the settings and login overlays, which I think require more advanced js

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -4,10 +4,35 @@ import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 
 import '../app.css';
 
+function SettingsOverlay(){
+	return(<div id="overlay">				
+		<div className = 'settingsform'>
+			<div className = 'overlaytitle'>
+				<div className ='backbutton'>
+					<a onclick = "document.getElementById('overlay').style.display = 'none'">
+						<img src = 'backbutton.png' alt="back" height="30px"/>
+					</a>
+				</div>
+				<h2> Settings </h2>
+			</div>
+			<fieldset className = "settingsq">
+				<h4>Ask for:</h4>
+				<input type="radio" id="askT" name="qa" value="askT"/>
+				<label for="askT">term</label>
+				<input type="radio" checked="checked" id="askD" name="qa" value="askD"/>
+				<label for="askD">definition</label>
+			</fieldset>
+			<div className = "settingsq">
+				<label for="sunsetOn">Allow sunrise/sunset notifications for your area?</label>
+				<input type="checkbox" id="sunsetOn" name="sunsetOn" className = "form-check-input"/>
+			</div>
+		</div>
+	</div>);
+}
 export function Play(){
 	return(
 		<div className = 'main'>
-
+		<SettingsOverlay />
 		<div className = 'sidebar'>
 			<div className = "sidebartext score">
 				<p>words left: 3 of 12, time: 5:01, rank: 15</p>
@@ -17,7 +42,7 @@ export function Play(){
 				<p> 5:33 pm - The sun is setting in your area!</p>
 			</div>
 			<div className = 'twobuttons'>
-				<button className ="btn btn-primary" onclick = "document.getElementById('overlay').style.display = 'flex'">
+				<button className ="btn btn-primary" onclick = "document.getElementById('overlay').style.display = 'block'">
 					<img src="/gear.svg" alt="settings logo" height='15pc' />
 				</button>
 				<div className = 'restartbutton'>
---
+++ src/scores/scores.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+
+import '../app.css';
+import './scorescreen.css';
+
+export function Scores(){
+	return(
+		<main>
+		<div className ="pagespace">
+			<div className ="personalscore">
+				<h2>Your top score: <span className ='scorenumber'>3:20</span></h2>
+				<p>putting you in <span className ='ranknumber'>8th</span> place overall</p>
+			</div>
+			
+			<table>
+				<tr>
+					<th>place
+					</th>
+					<th> user</th>
+					<th> time</th>
+				</tr>
+				<tr>
+					<td>1</td>
+					<td>jonsims</td>
+					<td>0:20</td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td>sjames</td>
+					<td>0:50</td>
+				</tr>
+				<tr>
+					<td>3</td>
+					<td>kayaklover15</td>
+					<td>1:20</td>
+				</tr>				<tr>
+					<td>4</td>
+					<td>mkblkwood</td>
+					<td>1:28</td>
+				</tr>				<tr>
+					<td>5</td>
+					<td>sjames02</td>
+					<td>1:54</td>
+				</tr>
+			</table>
+
+		</div>
+		</main>
+	);
+}
