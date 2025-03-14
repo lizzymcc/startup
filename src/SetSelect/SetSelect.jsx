1d708aecd06a9562e4e70481f30155e7a8b0418c
Lizzy McClellan <lizzymcc@byu.edu>
2025-03-14 14:35:28 -0600

    fixed a title change thing I commented out while debugging the deploy stuff

--- src/SetSelect/SetSelect.jsx
+++ src/SetSelect/SetSelect.jsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 import '../app.css';
 function SetDisp(props){
-	//console.log(props);
+	console.log(props);
 	return(
 		<div className = "setdisp">
 			<h3>
